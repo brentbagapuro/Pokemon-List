301ae82f509d3ade8debb688be99fd06bd7478b4
Brent Bagapuro <brentbagapuro@gmail.com>
2020-11-18 11:02:41 +0800

    added build

---
+++ build/precache-manifest.5eef70b5012d37d953297abada38123a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95c0ba1241fa70a691e6d1bb0088bcb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7cf0439c20f8e8e60594",
+    "url": "/static/css/main.b37f3e84.chunk.css"
+  },
+  {
+    "revision": "cc606426438c30d61f0e",
+    "url": "/static/js/2.6c9d114a.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/static/js/2.6c9d114a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cf0439c20f8e8e60594",
+    "url": "/static/js/main.85e61135.chunk.js"
+  },
+  {
+    "revision": "a0c7e88651a894b5141f",
+    "url": "/static/js/runtime-main.adb899fe.js"
+  },
+  {
+    "revision": "0e7cd608a008aa86c9ed3a2b988564f3",
+    "url": "/static/media/poke-bg.0e7cd608.jpg"
+  }
+]);
